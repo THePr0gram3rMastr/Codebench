9543ae4e189d27862446000ecdfd44a2713e35bb
J-Pascal Mercier <jean-pascal.mercier@inrialpes.fr>
2009-06-06 16:09:19 +0200

    Removed unused variables

--- ipc.py
+++ ipc.py
@@ -19,19 +19,6 @@ S_IRUSR = 00400
 S_IWUSR = 00200
 S_IROTH = 00004
 
-O_RDONLY = 00
-O_RDWR = 02
-
-PROT_READ = 0x1
-PROT_WRITE = 0x2
-PROT_EXEC = 0x4
-
-MAP_SHARED = 0x01
-MAP_PRIVATE = 0x02
-
-SEEK_END = 2
-SEEK_SET = 0
-
 mod = ctypes.CDLL(ctypes.util.find_library('rt'))
 mod.sem_open.restype = ctypes.c_void_p
 mod.sem_wait.argtypes = [ctypes.c_void_p]
@@ -52,53 +39,55 @@ sem_init = mod.sem_init
 
 
 class NamedSemaphore(object):
-	def __init__(self, name = None, create = False):
-		if name is None:
-			name = 'py' + codebench.aleatory.random_string(10)
-		self.owner = create
-		self.name = name
-		if create:
-			self.sem_p = sem_open(name, O_CREAT | O_EXCL, S_IROTH | S_IWUSR | S_IRUSR , 0)
-		else:
-			self.sem_p = sem_open(name, 0)
-		if self.sem_p is None:
-			raise RuntimeError()
-	
-	def __del__(self):
-		sem_close(self.sem_p)
-		if self.owner:
-			sem_unlink(self.name)
-	
-	def wait(self):
-		if 0 != sem_wait(self.sem_p):
-			raise RuntimeError()
-	
-	def post(self):
-		if 0 != sem_post(self.sem_p):
-			raise RuntimeError()
-	
-	def trywait(self):
-		return sem_trywait(self.sem_p) == 0
-	
-	def timedwait(self, sec):
-		abssec = calendar.timegm(time.gmtime()) + sec
-		return sem_timedwait(self.sem_p, ctypes.pointer(timespec(abssec, 0))) == 0
+        owner = False
+        name = ''
+        def __init__(self, name = None):
+                if name is None:
+                        name = 'py' + codebench.aleatory.random_string(10)
+                        self.owner = True
+                self.name = name
+                if self.owner:
+                        self.sem_p = sem_open(name, O_CREAT | O_EXCL, S_IROTH | S_IWUSR | S_IRUSR , 0)
+                else:
+                        self.sem_p = sem_open(name, 0)
+                if self.sem_p is None:
+                        raise RuntimeError()
+
+        def __del__(self):
+                sem_close(self.sem_p)
+                if self.owner:
+                        sem_unlink(self.name)
+
+        def wait(self):
+                if 0 != sem_wait(self.sem_p):
+                        raise RuntimeError()
+
+        def post(self):
+                if 0 != sem_post(self.sem_p):
+                        raise RuntimeError()
+
+        def trywait(self):
+                return sem_trywait(self.sem_p) == 0
+
+        def timedwait(self, sec):
+                abssec = calendar.timegm(time.gmtime()) + sec
+                return sem_timedwait(self.sem_p, ctypes.pointer(timespec(abssec, 0))) == 0
 
 
 
 class SemaphoreThread(threading.Thread):
-	def __init__(self, sem, wait = 2):
-		super(SemaphoreThread, self).__init__()
-		self.sem = sem
-		self.wtime = wait
-		self.stopevent = threading.Event()
-		self.semEvent = codebench.events.ThreadedEvent()
-	
-	def run(self):
-		while not self.stopevent.isSet():
-			if self.sem.timedwait(self.wtime):
-				self.semEvent()
-	
-	def stop(self):
-		self.stopevent.set()
+        def __init__(self, sem, wait = 2):
+                super(SemaphoreThread, self).__init__()
+                self.sem = sem
+                self.wtime = wait
+                self.stopevent = threading.Event()
+                self.semEvent = codebench.events.ThreadedEvent()
+
+        def run(self):
+                while not self.stopevent.isSet():
+                        if self.sem.timedwait(self.wtime):
+                                self.semEvent()
+
+        def stop(self):
+                self.stopevent.set()
 
