5143ccfd6210c4622d9122f0bd05d0663c45a4be
J-Pascal Mercier <jean-pascal.mercier@inrialpes.fr>
2009-12-18 10:30:04 +0100

    Added stats

---
+++ stats.py
@@ -0,0 +1,26 @@
+class IncrementalStatistic(object):
+	__n__ = 0
+	__mean__ = 0
+	__sumsq__ = 0
+	def __call__(self, x):
+		self.__n__ += 1
+		dev = x - self.__mean__
+		self.__mean__ = self.__mean__ + dev / float(self.__n__)
+		self.__sumsq__ += dev * (x - self.__mean__)
+
+	def __get_variance__(self):
+		return self.__sumsq__ / float(self.__n__)
+	var = property(__get_variance__)
+
+	def __get_mean__(self):
+		return self.__mean__
+	mean = property(__get_mean__)
+
+
+def incremental(n, mean, sumsq, x):
+	n += 1
+	dev = x - mean
+	mean = mean + dev / float(n)
+	sumsq += dev * (x - mean)
+	return n, mean , sumsq
+
